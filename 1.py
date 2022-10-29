390a8071439d7274ce64c4c04322205276195574
tugaybogdan <tugaybogdan8@gmail.com>
2022-10-29 15:42:14 +0300

    Add commit 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+def printing():
+    a=[str(x) for x in input().split()]
+    with open("file", 'w', encoding='UTF-8') as file:
+        for i in a:
+            file.write(str(i)+'\n')
+    return file
+printing()
+
